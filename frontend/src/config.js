081e73896d1c1dc2be4ea5627625394419566f4e
VladyslavOmelyanenko <36175376+VladyslavOmelyanenko@users.noreply.github.com>
2023-09-13 23:25:21 +0200

    update server url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,8 @@
 // const SERVER_URL = 'https://kb-strapi.onrender.com';
 
 
-const API_URL = 'http://16.171.206.171:1337/api';
-const SERVER_URL = 'http://16.171.206.171:1337/';
+const API_URL = 'https://cms.kyivbiennial.org/api';
+const SERVER_URL = 'https://cms.kyivbiennial.org.';
 
 
 export  {API_URL, SERVER_URL};
